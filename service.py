65ddbe53865522c10f07e0d4947ab0bca60b0032
Surajkumar9798 <me22b1073@iiitdm.ac.in>
2025-03-26 21:23:11 +0530

    bentomlfile is completed

--- service.py
+++ service.py
@@ -2,7 +2,7 @@ import numpy as np
 import bentoml
 from bentoml.io import NumpyNdarray
 
-iris_clf_runner= bentoml.sklearn.get("iris_clf:latest").to_runner()
+iris_clf_runner= bentoml.sklearn.get("iris_clf:latest").to_runner() 
 svc = bentoml.Service("iris_classifier", runners=   [iris_clf_runner])
 
 @svc.api(input=NumpyNdarray(), output= NumpyNdarray())
